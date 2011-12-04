e38f0b3978111001ebe6489d78cda66732897c7f
fentensoft <fentensoft@163.com>
2011-12-04 21:51:01 +0800

    CPU加一档

--- arch/arm/mach-omap2/board-mapphone.c
+++ arch/arm/mach-omap2/board-mapphone.c
@@ -161,6 +161,8 @@ struct omap_opp mapphone_omap3430_mpu_rate_table[] = {
 	{S550M, VDD1_OPP4, 0x38, 0x0},
 	/*OPP5*/
 	{S720M, VDD1_OPP5, 0x3E, 0x0},
+	/*OPP6*/
+	{S800M, VDD1_OPP6, 0x3E, 0x0},
 };
 
 #define S80M 80000000
@@ -187,7 +189,9 @@ static struct omap_opp mapphone_omap3430_dsp_rate_table[] = {
 	/*OPP4*/
 	{S400M, VDD1_OPP4, 0x38, 0x0},
 	/*OPP5*/
-	{S520M, VDD1_OPP5, 0x3E, 0x0},
+	{S430M, VDD1_OPP5, 0x38, 0x0},
+	/*OPP6*/
+	{S520M, VDD1_OPP6, 0x3E, 0x0},
 };
 
 static struct omap_opp mapphone_omap3630_mpu_rate_table[] = {
